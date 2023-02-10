be758edbb76b1e85f98fc53b800b49b45123719a
bradenacurtis801 <bradenacurtis801@gmail.com>
2023-02-10 11:15:20 -0700

    save

--- Module 3 - Programming Assignment IEEE/main.py
+++ Module 3 - Programming Assignment IEEE/main.py
@@ -6,6 +6,21 @@ import struct
 sys.float_info
 ### NaN -> '0x7ff8'
 
+def findIEEE(num):
+    exp = 0
+    mant = int(num)
+    while True:
+        if mant >= 2:
+            mant /=2
+            exp += 1
+        elif mant < 1:
+            mant *= 2
+            exp -=1
+        else:
+            if exp == 2047 and mant != 0:
+                return float('NaN')
+            return [mant, exp]
+
 def sign(x):
     """returns -1 if the x is negative, 0 if x is (either positive or negative) zero, 1 if x is positive. 
     """
@@ -21,32 +36,19 @@ def sign(x):
 def exponent(x):
     """returns the unbiased (true) binary exponent of x as a decimal integer. Remember that 
     subnormals are a special case. Consider 0 to be a subnormal. """
-    exp = 0
-    mant = x
-    while True:
-        if mant >= 2:
-            mant /=2
-            exp += 1
-        elif mant < 1:
-            mant *= 2
-            exp -=1
-        else:
-            if exp == 2047 and mant != 0:
-                return float('NaN')
-            exp = struct.unpack('Q', struct.pack('d', x))
-            return [mant, exp]
+    return findIEEE(x)[1]
  
 def fraction(x):
     """returns the IEEE fractional part of x as a decimal floating-point number. You must convert 
-    binary to decimal. The fraction portion does not include the leading 1 that is not stored. 
-""" 
-pass
+    binary to decimal. The fraction portion does not include the leading 1 that is not stored. """ 
+    y = 0
+    frac = findIEEE(x)[0]
+    return frac
 
 def mantissa(x): 
     """returns the full IEEE mantissa of x as a decimal floating-point number (which is the same as 
-    fraction() + 1  for normalized numbers; same as fraction() for subnormals). 
-""" 
-pass
+    fraction() + 1  for normalized numbers; same as fraction() for subnormals). """ 
+    return findIEEE(x)[0]
 
 def is_posinfinity(x):
     """returns true if x is positive infinity """
@@ -73,31 +75,31 @@ def ulps(x, y) :
 pass
       
 def main():
-    # y = 6.5 
-    # subMin = np.nextafter(0,1) //subMin = 5e-324 
-    # print(sign(y)) //1 
-    # print(sign(0.0)) // 0 
-    # print(sign(-y)) // -1 
-    # print(sign(-0.0)) //0 
-    # print(exponent(y)) // 2 
-    # print(exponent(16.6)) // 4 
-    # print(fraction(0.0)) //0.0 
-    # print(mantissa(y)) //1.625 
-    # print(mantissa(0.0) //0.0 
+    y = 6.5 
+    # subMin = np.nextafter(0,1) ##subMin = 5e-324 
+    # print(sign(y)) ##1 
+    # print(sign(0.0)) ## 0 
+    # print(sign(-y)) ## -1 
+    # print(sign(-0.0)) ##0 
+    print(exponent(y)) ## 2 
+    print(exponent(16.6)) ## 4 
+    print(fraction(0.0)) ##0.0
+    print(mantissa(y)) ##1.625 
+    print(mantissa(0.0)) ##0.0 
     # var1 = float(‘nan’) 
-    # print(exponent(var1)) // 1024 
-    # print(exponent(0.0) // 0 
-    # print(exponent(subMin)) // -1022 
-    # print(is_posinfinity(math.inf)) // True 
-    # print(is_neginfinity(math.inf)) // False 
-    # print(not is_posinfinity(-math.inf)) //True 
-    # print(is_neginfinity(-math.inf)) //True 
-    # print(ulp(y)) // 8.881784197001252e-16 
-    # print(ulp(1.0)) // 2.220446049250313e-16 
-    # print(ulp(0.0)) // 5e-324 
-    # print(ulp(subMin)) // 5e-324 
-    # print(ulp(1.0e15)) // 0.125 
-    # print(ulps(1,2)) // 4503599627370496 
+    # print(exponent(var1)) ## 1024 
+    # print(exponent(0.0) ## 0 
+    # print(exponent(subMin)) ## -1022 
+    # print(is_posinfinity(math.inf)) ## True 
+    # print(is_neginfinity(math.inf)) ## False 
+    # print(not is_posinfinity(-math.inf)) ##True 
+    # print(is_neginfinity(-math.inf)) ##True 
+    # print(ulp(y)) ## 8.881784197001252e-16 
+    # print(ulp(1.0)) ## 2.220446049250313e-16 
+    # print(ulp(0.0)) ## 5e-324 
+    # print(ulp(subMin)) ## 5e-324 
+    # print(ulp(1.0e15)) ## 0.125 
+    # print(ulps(1,2)) ## 4503599627370496 
 
     
 
